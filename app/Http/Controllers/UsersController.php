96603db79f818820a3b3dae03651514d6bfd627b
zxdd5 <zxdd5@163.com>
2017-06-08 17:06:55 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -35,7 +36,8 @@ class UsersController extends Controller
           'email' => $request->email,
           'password' => bcrypt($request->password),
       ]);
-          session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+      Auth::login($user);
+    session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+    return redirect()->route('users.show', [$user]);
     }
 }

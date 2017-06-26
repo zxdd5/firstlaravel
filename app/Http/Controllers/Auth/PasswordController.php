ef203572162c5b0c3ea9585689a32ebe22c1f5b3
zxdd5 <zxdd5@163.com>
2017-06-26 11:48:38 +0800

    account

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Auth;
+
 
 class PasswordController extends Controller
 {
@@ -19,6 +26,7 @@ class PasswordController extends Controller
     */
 
     use ResetsPasswords;
+    protected $redirectPath = '/';
 
     /**
      * Create a new password controller instance.
@@ -28,5 +36,23 @@ class PasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+    public function postEmail(Request $request){
+      $this->validate($request, ['email' => 'required|email|max:255']);
+      $user = User::where('email',$request->email)->firstOrFail();
+      if($user->activated==0){
+        return redirect()->back()->with('status', '该邮箱未激活,请先激活在修改密码');
+      }
+      $response = Password::sendResetLink($request->only('email'), function (Message $message) {
+            $message->subject($this->getEmailSubject());
+        });
+
+        switch ($response) {
+            case Password::RESET_LINK_SENT:
+                return redirect()->back()->with('status', trans($response));
+            case Password::INVALID_USER:
+                return redirect()->back()->withErrors(['email' => trans($response)]);
+        }
+
     }
 }
